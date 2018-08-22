e78e60e556f539ed3622a8ae699cefe22afd21fb
Chris Alberti <christopherjalberti@gmail.com>
2018-08-22 16:59:03 -0400

    add session#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: params[:username])
 
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def destroy
-    binding.pry
     session.clear
     redirect_to "/"
   end
