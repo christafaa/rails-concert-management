bfb693ac659297112dcb3c3b1ff879ffc085f031
Chris Alberti <christopherjalberti@gmail.com>
2018-08-22 14:13:49 -0400

    finish writing migrations

--- db/migrate/20180822171201_create_concerts.rb
+++ db/migrate/20180822171201_create_concerts.rb
@@ -1,6 +1,9 @@
 class CreateConcerts < ActiveRecord::Migration[5.2]
   def change
     create_table :concerts do |t|
+      t.string :title
+      t.string :venue
+      t.string :date
     end
   end
 end
--- db/migrate/20180822171210_create_tickets.rb
+++ db/migrate/20180822171210_create_tickets.rb
@@ -1,6 +1,9 @@
 class CreateTickets < ActiveRecord::Migration[5.2]
   def change
     create_table :tickets do |t|
+      t.integer :concert_id
+      t.integer :attendee_id
+      t.integer :seat_number
     end
   end
 end
