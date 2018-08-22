902e2e722f073f081e0eafd5017c638735b86889
Chris Alberti <christopherjalberti@gmail.com>
2018-08-22 14:22:02 -0400

    add model associations

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,3 +1,5 @@
 class Attendee < ActiveRecord::Base
-
+  belongs_to :user
+  has_many :tickets
+  has_many :concerts, through: :tickets
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,4 @@
 class Ticket < ActiveRecord::Base
-
+  belongs_to :concert
+  belongs_to :attendee
 end
