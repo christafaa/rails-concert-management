44b3895b81a0108e4af650e7763f08d934345402
Chris Alberti <christopherjalberti@gmail.com>
2018-08-22 22:51:25 -0400

    add attendee controller and view files

---
+++ app/controllers/attendees_controller.rb
@@ -0,0 +1,35 @@
+class AttendeesController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def attendee_params
+
+  end
+end
