497b743d2ad4980656458fe63445b8fced06739a
Chris Alberti <christopherjalberti@gmail.com>
2018-08-22 23:51:35 -0400

    add attendee#new and attendee#create

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,10 +1,16 @@
 class AttendeesController < ApplicationController
   def new
-
+    @attendee = Attendee.new
   end
 
   def create
+    @attendee = Attendee.create(attendee_params)
 
+    if @attendee.save
+      redirect_to attendee_path(@attendee)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -30,6 +36,6 @@ class AttendeesController < ApplicationController
   private
 
   def attendee_params
-
+    params.require(:attendee).permit(:name, :profession, :age, :wealth_rating, :notes, :user_id)
   end
 end
