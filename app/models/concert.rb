e767e08d4bdd62dcbb780def41f764e6b3c5f9ca
Chris Alberti <christopherjalberti@gmail.com>
2018-08-29 21:24:48 -0400

    refactor model validations

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -2,8 +2,6 @@ class Concert < ActiveRecord::Base
   has_many :tickets
   has_many :attendees, through: :tickets
 
-  validates :title, presence: true
+  validates :title, :venue, :date, presence: true
   validates :title, uniqueness: true
-  validates :venue, presence: true
-  validates :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   has_many :attendees
 
-  validates :username, presence: true
+  validates :username, :password, presence: true
   validates :username, uniqueness: true
-  validates :password, presence: true
   has_secure_password
 end
