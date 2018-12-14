bd22696cfbd15c6feffe65ebd0ad85a09dd4ec9a
Chris Alberti <christopherjalberti@gmail.com>
2018-12-13 21:52:06 -0500

    install jquery and active model serializers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,6 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require_attendees
+//= require jquery3
+//= require jquery_ujs
---
+++ app/assets/javascripts/attendees.js
@@ -0,0 +1,44 @@
+class Attendee {
+  constructor(data) {
+    this.first_name = data.first_name;
+    this.last_name = data.last_name;
+    this.profession = data.profession;
+    this.age = data.age;
+    this.wealth_rating = data.wealth_rating;
+    this.notes = data.notes;
+    this.user_id = data.user_id;
+  }
+
+  displayName() {
+    return `${this.first_name} ${this.last_name}`
+  }
+}
+
+$(function() {
+  addSortListener();
+});
+
+function addSortListener() {
+  $("div#sort-button form").on("submit", function(e) {
+    e.preventDefault();
+    alert("hi");
+    // getConcerts();
+  })
+}
+
+// function appendAttendees(data) {
+//   let attendees = [];
+//   let unique = true;
+//   data.forEach(function(attendeeData) {
+//     let newAttendee = new Attendee(attendeeData)
+//     attendees.forEach(function(attendee){
+//       if (attendee.displayName() === newAttendee.displayName()) {
+//         unique = false;
+//       }
+//     });
+//     if (unique) {
+//       attendees.push(newAttendee)
+//     }
+//   });
+//   $("#display").append(HandlebarsTemplates['attendees_template'](attendees));
+// }
